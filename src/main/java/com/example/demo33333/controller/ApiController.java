406f3c0d1781f873ab184aec1261bf8fdc5a1284
klchib3 <klchib3@gmail.com>
2024-02-04 13:38:28 +0800

    155

--- src/main/java/com/example/demo33333/controller/ApiController.java
+++ src/main/java/com/example/demo33333/controller/ApiController.java
@@ -9,7 +9,7 @@ public class ApiController {
 	// TODO xxxxxxx
 	@GetMapping("/test1")
 	public String aa() {
-		return "cccccccttt15";
+		return "cccccccttt155";
 	}
 	@GetMapping("/test11")
 	public String aaa() {
